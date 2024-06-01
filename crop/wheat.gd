80e71057c91f492ebe5f262c7323b5c1e0f80d5e
marko.radoicic1 <marko.radoicic1@gmail.com>
2024-06-01 22:38:56 +0200

    lol lemi

--- crop/wheat.gd
+++ crop/wheat.gd
@@ -1,20 +1,19 @@
-extends Node2D
+extends Item
 class_name Wheat
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var count = 1
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_Interactable_interacted_with(player : Player):
+	print("Wheat")
+	if player.item.item_type == Item.ItemType.WHEAT:
+		print("Stacked")
+		player.item.count += count
+		player.current_interactable = null
+		queue_free()
+	else:
+		player.equip(self)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Interactable_interacted_with(player:Player):
-	pass # Replace with function body.
+func _process(delta):
+	$Label.text = str(count)
