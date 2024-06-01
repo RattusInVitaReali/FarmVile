61f24afb4f38b011ec993dde942fed92e04dfabf
LuTheDog <lukamatalzic@gmail.com>
2024-06-01 17:20:28 +0200

    finish bucket

--- item/bucket.gd
+++ item/bucket.gd
@@ -4,15 +4,21 @@ class_name BUCKET
 export var water_level = 100
 export(int) var water_level_decrement = 5
 
+var empty_texture: Texture
+var full_texture: Texture
 
 func use():
-	water_level -= water_level_decrement
+	if water_level > 0:
+		water_level -= water_level_decrement
+	update_sprite()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	empty_texture = load("res://item/sprites/bucket_empty.png")
+	full_texture = load("res://item/sprites/bucket_full.png")
+	
+func update_sprite():
+	if water_level <= 0:
+		item_texture = empty_texture
+	else:
+		item_texture = full_texture
