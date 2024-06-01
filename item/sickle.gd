eb36fdd206f3aef00f86dfbe5d0d2fd26c424810
LuTheDog <lukamatalzic@gmail.com>
2024-06-01 21:36:31 +0200

    sickle

--- item/sickle.gd
+++ item/sickle.gd
@@ -1,5 +1,5 @@
-extends Node2D
-class_name Wheat
+extends Item
+class_name Sickle
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,13 +8,8 @@ class_name Wheat
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	item_type = ItemType.SICKLE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Interactable_interacted_with(player:Player):
-	pass # Replace with function body.
